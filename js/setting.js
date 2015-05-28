4029c6af1e4a4ac54cd98a2ed5900751b3f51303
matsusakahon <hr.morimoto@city.matsusaka.mie.jp>
2015-05-29 08:40:10 +0900

    Update setting.js

--- js/setting.js
+++ js/setting.js
@@ -7,6 +7,6 @@ const SVGLabel = false; // SVGイメージを使用するときは、true。用�
 
 const MaxDescription = 9; // ごみの最大種類、９を超えない場合は変更の必要はありません。
 
-const MaxMonth = 12;
+const MaxMonth = 3;
 
 const WeekShift = false; // 休止期間なら週をずらすときは、true。金沢の仕様は、true。
